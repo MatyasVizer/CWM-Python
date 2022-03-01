2c1d7f6b4d0de43bcf2105b911cbafcc8fb0c110
MatyasVizer <matyasvizer@gmail.com>
2022-03-01 13:07:18 +0100

    added notes

--- notes.py
+++ notes.py
@@ -2,8 +2,8 @@
 
 # type annotation(conflict)
 
-x: int = 1
-x = "Sample John"  # <<<<<this will throw a runtime error
+#x: int = 1
+# x = "Sample John"  # <<<<<this will throw a runtime error
 
 # mutable
 
@@ -20,7 +20,7 @@ z.append(4)
 print(id(z))
 
 
-# strings
+# 5: strings
 
 course = "Python Course"
 print(len(course))
@@ -30,3 +30,50 @@ print(course[0:3])
 print(course[:3])  # passes zero as start index on default
 print(course[0:])  # passes end index on default
 print(course[:])  # passes the defualt values
+
+
+# 6: escape sequences
+
+message = 'Python "Programming'
+print(message)
+
+message = "Python\"Programming"
+print(message)
+
+message = "Python\\Programming"
+print(message)
+
+message = "Python\nProgramming"
+print(message)
+
+message = """Python
+Programming
+"""
+print(message)
+
+
+# 7: formatted strings
+
+first = "Matyas"
+last = "Vizer"
+full = f"{first} {last}"
+full2 = f"{len(first)} {2 + 18}"
+print(full)
+print(full2)
+
+
+# 8: useful string methods for string objects
+
+print(course.upper())
+print(course.lower())
+print(course.title())
+
+print(course.strip())  # deletes whitespaces
+print(course.lstrip())  # deletes whitespaces at the beginning called left strip
+print(course.rstrip())  # deletes whitespaces at the end called right strip
+
+print(course.find("pro"))
+print(course.replace("P", "-"))
+
+print("Programming" in course)
+print("Programming" not in course)
