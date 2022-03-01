8a2193ce8156ea03df1bc47068b4dd76f9f3ce63
MatyasVizer <matyasvizer@gmail.com>
2022-03-02 00:55:26 +0100

    fixed capital chars

--- Python Basics/exercise.py
+++ Python Basics/exercise.py
@@ -1,13 +1,13 @@
 def fizz_buzz(input):
     message = ""
     if input % 3 == 0 and input % 5 != 0:
-        message = "fizz"
+        message = "Fizz"
         return message
     elif input % 5 == 0 and input % 3 != 0:
-        message = "buzz"
+        message = "Buzz"
         return message
     elif input % 3 == 0 and input % 5 == 0:
-        message = "fizzbuzz"
+        message = "FizzBuzz"
         return message
     else:
         return input
