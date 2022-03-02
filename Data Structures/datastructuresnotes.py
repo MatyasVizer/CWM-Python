af4ff72b232f320dd5a2dc272ff33e0bbbad23cb
MatyasVizer <matyasvizer@gmail.com>
2022-03-02 11:52:02 +0100

    sorting algorithm

--- Data Structures/datastructuresnotes.py
+++ Data Structures/datastructuresnotes.py
@@ -89,3 +89,23 @@ if "d" in letters:
     print(letters.index("d"))
 
 # 7: sorting lists
+
+numbers = [3, 51, 2, 8, 6]
+# numbers.sort()
+new_numbers = sorted(numbers, reverse=True)
+print(numbers)
+print(new_numbers)
+
+items = [
+    ("Product1", 10),
+    ("Product2", 9),
+    ("Product3", 12),
+]
+
+
+def sort_item(item):
+    return item[1]
+
+
+items.sort(key=sort_item)
+print(items)
