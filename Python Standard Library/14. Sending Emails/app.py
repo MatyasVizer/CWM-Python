1251c86a15c990f0e9499718b9cd9f8d4f5c822b
MatyasVizer <matyasvizer@gmail.com>
2022-03-07 12:32:28 +0100

    finished smtp server/emails

---
+++ Python Standard Library/14. Sending Emails/app.py
@@ -0,0 +1,20 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from pathlib import Path
+import smtplib
+
+
+message = MIMEMultipart()
+message["from"] = "test"
+message["to"] = "test@gmail.com"
+message["subject"] = "This is a test"
+message.attach(MIMEText("Body"))
+message.attach(MIMEImage(Path("test.png").read_bytes))
+
+with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()  # transport layer security
+    smtp.login("address", "password")
+    smtp.send_message(message)
+    print("Sent...")
