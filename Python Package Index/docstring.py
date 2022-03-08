9c66083e9c521616378dbcc7d407c0126ff350ee
MatyasVizer <matyasvizer@gmail.com>
2022-03-08 11:50:12 +0100

    finished

---
+++ Python Package Index/docstring.py
@@ -0,0 +1,19 @@
+""" This module provides functions to convert a PDF to text"""
+
+
+# first line should be short description
+# then blank line
+# then detailed description
+
+class Converter:
+    """ A simple converter for converting PDFs to text"""
+    def convert():
+        """ Convert the given PDF to text.
+
+        Parameters:
+        path(str): the path to a PDF file.
+
+        Returns:
+        str: the content of the PDF file as a text.
+        """
+        print("pdf2text")
